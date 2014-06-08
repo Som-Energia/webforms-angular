6b594c033f9d35eef36bceed45470f593e006f75
David Romaní <david@flux.cat>
2014-06-08 21:22:48 +0200

    add translations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,35 @@
 'use strict';
 
 angular
-  .module('newSomEnergiaWebformsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
+    .module('newSomEnergiaWebformsApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'pascalprecht.translate'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .config(function($translateProvider) {
+        $translateProvider
+            .translations('en', {
+                HEADLINE: 'Hello there, This is my awesome app!',
+                INTRO_TEXT: 'And it has i18n support!'
+            });
+    })
     .constant('cfg', {
-    API_BASE_URL: 'http://somenergia-api-webforms.gisce.net/',
-    STATUS_OFFLINE: 'OFFLINE',
-    STATUS_ONLINE: 'ONLINE',
-    STATE_TRUE: true,
-    STATE_FALSE: false
-  })
+        API_BASE_URL: 'http://somenergia-api-webforms.gisce.net/',
+        STATUS_OFFLINE: 'OFFLINE',
+        STATUS_ONLINE: 'ONLINE',
+        STATE_TRUE: true,
+        STATE_FALSE: false
+    })
 ;
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,40 +1,40 @@
 'use strict';
 
 angular.module('newSomEnergiaWebformsApp')
-  .controller('MainCtrl', ['cfg', '$scope', '$http', '$log', function (cfg, $scope, $http, $log) {
+    .controller('MainCtrl', ['cfg', '$scope', '$http', '$log', function (cfg, $scope, $http, $log) {
 
         // GET STATES
-        $http.get(cfg.API_BASE_URL + 'data/provincies').success(function(response) {
+        $http.get(cfg.API_BASE_URL + 'data/provincies').success(function (response) {
                 if (response.status === cfg.STATUS_ONLINE) {
-                  if (response.state === cfg.STATE_TRUE) {
-                    $scope.provinces = response.data.provincies;
-                  } else {
-                    $log.error('data/provinces error response recived', response);
-                    $scope.showErrorDialog('GET provincies return false state');
-                  }
+                    if (response.state === cfg.STATE_TRUE) {
+                        $scope.provinces = response.data.provincies;
+                    } else {
+                        $log.error('data/provinces error response recived', response);
+                        $scope.showErrorDialog('GET provincies return false state');
+                    }
                 } else if (response.status === cfg.STATUS_OFFLINE) {
-                  $scope.showErrorDialog('API server status offline');
+                    $scope.showErrorDialog('API server status offline');
                 } else {
-                  $scope.showErrorDialog('API server unknown status');
+                    $scope.showErrorDialog('API server unknown status');
                 }
-              }
+            }
         );
 
         // GET LANGUAGES
-        $http.get(cfg.API_BASE_URL + 'data/idiomes').success(function(response) {
+        $http.get(cfg.API_BASE_URL + 'data/idiomes').success(function (response) {
                 if (response.status === cfg.STATUS_ONLINE) {
-                  if (response.state === cfg.STATE_TRUE) {
-                    $scope.languages = response.data.idiomes;
-                  } else {
-                    $log.log('data/idiomes error response recived', response);
-                    $scope.showErrorDialog('GET idiomes return false state');
-                  }
+                    if (response.state === cfg.STATE_TRUE) {
+                        $scope.languages = response.data.idiomes;
+                    } else {
+                        $log.log('data/idiomes error response recived', response);
+                        $scope.showErrorDialog('GET idiomes return false state');
+                    }
                 } else if (response.status === cfg.STATUS_OFFLINE) {
-                  $scope.showErrorDialog('API server status offline');
+                    $scope.showErrorDialog('API server status offline');
                 } else {
-                  $scope.showErrorDialog('API server unknown status');
+                    $scope.showErrorDialog('API server unknown status');
                 }
-              }
+            }
         );
 
         // INIT
@@ -49,45 +49,45 @@ angular.module('newSomEnergiaWebformsApp')
         $scope.city = {};
 
         // ON CHANGE SELECTED STATE
-        $scope.updateSelectedCity = function() {
+        $scope.updateSelectedCity = function () {
             // GET CITIES
-            $http.get(cfg.API_BASE_URL + 'data/municipis/' + $scope.province.id).success(function(response) {
+            $http.get(cfg.API_BASE_URL + 'data/municipis/' + $scope.province.id).success(function (response) {
                     if (response.status === cfg.STATUS_ONLINE) {
-                      if (response.state === cfg.STATE_TRUE) {
-                        $scope.cities = response.data.municipis;
-                      } else {
-                        $log.log('data/municipis/' + $scope.province.id + ' response recived', response);
-                        $scope.showErrorDialog('GET municipis return false state');
-                      }
+                        if (response.state === cfg.STATE_TRUE) {
+                            $scope.cities = response.data.municipis;
+                        } else {
+                            $log.log('data/municipis/' + $scope.province.id + ' response recived', response);
+                            $scope.showErrorDialog('GET municipis return false state');
+                        }
                     } else if (response.status === cfg.STATUS_OFFLINE) {
-                      $scope.showErrorDialog('API server status offline');
+                        $scope.showErrorDialog('API server status offline');
                     } else {
-                      $scope.showErrorDialog('API server unknown status');
+                        $scope.showErrorDialog('API server unknown status');
                     }
-                  }
+                }
             );
-          };
+        };
 
         // ON SUBMIT FORM
-        $scope.submit = function(form) {
+        $scope.submit = function (form) {
             // Trigger validation flag.
             $scope.submitted = true;
 
             // If form is invalid, return and let AngularJS show validation errors.
             if (form.$invalid) {
-              return null;
+                return null;
             }
 
             return true;
-          };
+        };
 
         // SHOW ERROR MODAL DIALOG
-        $scope.showErrorDialog = function(msg) {
-          $scope.errorMsg = msg;
-          jQuery('#api-server-offline-modal').modal({
+        $scope.showErrorDialog = function (msg) {
+            $scope.errorMsg = msg;
+            jQuery('#api-server-offline-modal').modal({
 //            backdrop: 'static', TODO uncomment this
-            keyboard: false,
-            show: true
-          });
+                keyboard: false,
+                show: true
+            });
         };
-      }]);
+    }]);
