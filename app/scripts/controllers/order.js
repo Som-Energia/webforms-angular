e903fe3dc5bab3a2d9bed835eed146c07938f9e7
David Romaní <david@flux.cat>
2014-06-09 21:30:12 +0200

    add new order form skeleton 3 - include fragments

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -4,5 +4,12 @@ angular.module('newSomEnergiaWebformsApp')
     .controller('OrderCtrl', ['cfg', '$scope', '$http', '$routeParams', '$translate', '$log', function (cfg, $scope, $http, $routeParams, $translate, $log) {
 
 
+        // INIT
+        $scope.initSubmitReady = false;
+
+        // ON CHANGE FORMS
+        $scope.initFormListener = function (form) {
+            $scope.initSubmitReady = $scope.form.init.dni !== undefined && $scope.form.init.socinumber !== undefined;
+        };
 
     }]);
