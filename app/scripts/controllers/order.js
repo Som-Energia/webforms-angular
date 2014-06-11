1a6bec9ab3bbcb1cadcc46a70a86183ec63c5516
David Romaní <david@flux.cat>
2014-06-11 16:40:18 +0200

    form contractacio step 0

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('newSomEnergiaWebformsApp')
-    .controller('OrderCtrl', ['cfg', 'AjaxHandler', 'ValidateHandler', '$scope', '$http', '$routeParams', '$translate', '$timeout', '$window', '$log', function (cfg, AjaxHandler, ValidateHandler,  $scope, $http, $routeParams, $translate, $timeout, $window, $log) {
+    .controller('OrderCtrl', ['cfg', 'AjaxHandler', 'ValidateHandler', 'uiHandler', '$scope', '$http', '$routeParams', '$translate', '$timeout', '$window', '$log', function (cfg, AjaxHandler, ValidateHandler, uiHandler, $scope, $http, $routeParams, $translate, $timeout, $window, $log) {
 
         // INIT
         $scope.dniIsInvalid = false;
@@ -9,9 +9,38 @@ angular.module('newSomEnergiaWebformsApp')
         $scope.showStep1Form = false;
         $scope.initSubmitReady = false;
         $scope.initFormSubmitted = false;
+        $scope.languages = [];
+        $scope.provinces = [];
+        $scope.cities = [];
         if ($routeParams.locale !== undefined) {
             $translate.use($routeParams.locale);
         }
+
+        // GET LANGUAGES
+        var languagesPromise = AjaxHandler.getDataRequest($scope, cfg.API_BASE_URL + 'data/idiomes', '002');
+        languagesPromise.then(
+            function (response) {
+                if (response.state === cfg.STATE_TRUE) {
+                    $scope.languages = response.data.idiomes;
+                } else {
+                    uiHandler.showErrorDialog('GET response state false recived (ref.003-002)');
+                }
+            },
+            function (reason) { $log.error('Failed', reason); }
+        );
+
+        // GET STATES
+        var statesPromise = AjaxHandler.getDataRequest($scope, cfg.API_BASE_URL + 'data/provincies', '001');
+        statesPromise.then(
+            function (response) {
+                if (response.state === cfg.STATE_TRUE) {
+                    $scope.provinces = response.data.provincies;
+                } else {
+                    uiHandler.showErrorDialog('GET response state false recived (ref.003-001)');
+                }
+            },
+            function (reason) { $log.error('Failed', reason); }
+        );
 
         // DNI VALIDATION
         var checkDniTimer = false;
