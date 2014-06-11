0a73dcfb4c925764e662c245b07fe86795f423a2
David Romaní <david@flux.cat>
2014-06-11 21:08:19 +0200

    order form step 1 - 2

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -5,14 +5,18 @@ angular.module('newSomEnergiaWebformsApp')
 
         // INIT
         $scope.dniIsInvalid = false;
+        $scope.cupsIsInvalid = false;
+        $scope.cnaeIsInvalid = false;
         $scope.showUnknownSociWarning = false;
         $scope.showBeginOrderForm = false;
         $scope.showStep1Form = false;
         $scope.initSubmitReady = false;
         $scope.initFormSubmitted = false;
+        $scope.orderFormSubmitted = false;
         $scope.languages = [];
         $scope.provinces = [];
         $scope.cities = [];
+        $scope.form = {};
         if ($routeParams.locale !== undefined) {
             $translate.use($routeParams.locale);
         }
@@ -63,6 +67,46 @@ angular.module('newSomEnergiaWebformsApp')
             }, 400);
         });
 
+        // CUPS VALIDATION
+        var checkCupsTimer = false;
+        $scope.$watch('form.cups', function(newValue) {
+            if (checkCupsTimer) {
+                $timeout.cancel(checkCupsTimer);
+            }
+            checkCupsTimer = $timeout(function() {
+                if (newValue !== undefined) {
+                    var cupsPromise = AjaxHandler.getSateRequest($scope, cfg.API_BASE_URL + 'check/cups/' + newValue, '006');
+                    cupsPromise.then(
+                        function (response) {
+                            $scope.cupsIsInvalid = response === cfg.STATE_FALSE;
+                            $scope.formListener($scope.form);
+                        },
+                        function (reason) { $log.error('Failed', reason); }
+                    );
+                }
+            }, 400);
+        });
+
+        // CNAE VALIDATION
+        var cnaeCupsTimer = false;
+        $scope.$watch('form.cnae', function(newValue) {
+            if (cnaeCupsTimer) {
+                $timeout.cancel(cnaeCupsTimer);
+            }
+            cnaeCupsTimer = $timeout(function() {
+                if (newValue !== undefined) {
+                    var cnaePromise = AjaxHandler.getSateRequest($scope, cfg.API_BASE_URL + 'check/cnae/' + newValue, '007');
+                    cnaePromise.then(
+                        function (response) {
+                            $scope.cnaeIsInvalid = response === cfg.STATE_FALSE;
+                            $scope.formListener($scope.form);
+                        },
+                        function (reason) { $log.error('Failed', reason); }
+                    );
+                }
+            }, 400);
+        });
+
         // ON CHANGE FORMS
         $scope.formListener = function (form) {
             $scope.initSubmitReady = form.dni !== undefined && form.socinumber !== undefined && $scope.dniIsInvalid === false;
@@ -97,6 +141,25 @@ angular.module('newSomEnergiaWebformsApp')
 
         $scope.initOrderForm = function() {
             $scope.showStep1Form = true;
+        };
+
+        // ON CHANGE SELECTED PROVINCE
+        $scope.updateSelectedCity = function (form) {
+            if ($scope.form.province !== undefined) {
+                // GET CITIES
+                var citiesPromise = AjaxHandler.getDataRequest($scope, cfg.API_BASE_URL + 'data/municipis/' +  $scope.form.province.id, '003');
+                citiesPromise.then(
+                    function (response) {
+                        if (response.state === cfg.STATE_TRUE) {
+                            $scope.cities = response.data.municipis;
+                        } else {
+                            uiHandler.showErrorDialog('GET response state false recived (ref.003-003)');
+                        }
+                    },
+                    function (reason) { $log.error('Failed', reason); }
+                );
+                $scope.formListener(form);
+            }
         };
 
     }]);
