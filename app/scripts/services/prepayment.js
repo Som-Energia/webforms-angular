1635d76228c04e562f39851bd1a2f5928404d754
David Romaní <david@flux.cat>
2014-07-04 21:26:28 +0200

    remove fake payment data from service

--- app/scripts/services/prepayment.js
+++ app/scripts/services/prepayment.js
@@ -3,21 +3,7 @@
 angular.module('newSomEnergiaWebformsApp')
     .service('prepaymentService', ['$log', function($log) {
 
-        // TODO: remove fake data in production
-        var data = {
-            endpoint: 'https://www.arquia.es/ArquiaRed/pgateway.aspx',
-            payment_data: {
-                CONC1: 'QUOTA SOCI',
-                CONF: '0100',
-                DNI_CLI: '13572468F',
-                ID_OPERACION: 'dggokFcmcdq27D6f0A9ssJLzfZzYUH3',
-                ID_USU: '37',
-                IMPORTE: '100',
-                NOMBRE_CLI: 'USUARIO DE PRUEBAS',
-                REF: '2014f9d5b0d7'
-            },
-            payment_type: 'rebut'
-        };
+        var data;
 
         // SETTER
         var setData = function(pData) {
