84c2a6c1594710701021b3e461123fadf22d8526
David García Garzón <voki@canvoki.net>
2015-07-07 19:07:35 +0200

    generationstats wip

---
+++ wp/generationstats.js
@@ -0,0 +1,83 @@
+'use strict';
+
+alert('Benvingut!');
+
+if (jQuery) {
+    setCustomLocaleToStringBehaviour();
+    jQuery(document).ready(function() {
+        // Partners
+        jQuery.getJSON('https://api.somenergia.coop/stats/generation_amount', function(response) {
+            var node = jQuery('#partners');
+            if (response.status === 'OFFLINE') {
+                node.text('no disponible (EP1)');
+            } else {
+                if (response.state === false) {
+                    node.text('no disponible (EP2)');
+                } else {
+                    if (response.data === undefined) {
+                        node.text('no disponible (EP3)');
+                    } else {
+                        node.text(Number(response.data.socis).customToLocaleString());
+                    }
+                }
+            }
+        });
+        // Contracts
+        jQuery.getJSON('https://api.somenergia.coop/stats/contractes', function(response) {
+            var node = jQuery('#contracts');
+            if (response.status === 'OFFLINE') {
+                node.text('no disponible (EC1)');
+            } else {
+                if (response.state === false) {
+                    node.text('no disponible (EC2)');
+                } else {
+                    if (response.data === undefined) {
+                        node.text('no disponible (EC3)');
+                    } else {
+                        node.text(Number(response.data.contractes).customToLocaleString());
+                    }
+                }
+            }
+        });
+    });
+} else {
+    console.error('Unable to load jQuery');
+}
+
+function browserSupportsToLocaleString() {
+    var number = 0;
+    try {
+        number.toLocaleString('i');
+    } catch (e) {
+        return e.name === 'RangeError';
+    }
+    return false;
+}
+
+function setCustomLocaleToStringBehaviour()
+{
+    if (browserSupportsToLocaleString()) {
+        Number.prototype.customToLocaleString = function() {
+            return this.toLocaleString();
+        }
+    } else {
+        Number.prototype.customToLocaleString = function() {
+            var stringValue = Math.round(this).toString();
+            var resultValue = '';
+            for (var i = 0, len = stringValue.length; i < len; i++) {
+                var inversedIndex = stringValue.length - i - 1;
+                if ((i + 1) % 3 === 0 && inversedIndex !== 0) {
+                    resultValue = resultValue + stringValue[inversedIndex] + '.';
+                } else {
+                    resultValue = resultValue + stringValue[inversedIndex];
+                }
+            }
+            stringValue = '';
+            for (i = resultValue.length - 1; i >= 0; i--) {
+                stringValue += resultValue[i];
+            }
+            
+            return stringValue;
+        }
+    }
+}
