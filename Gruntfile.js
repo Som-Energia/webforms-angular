4d3962d61e48a4c7de6d55f5e9673b8ee83fa27d
David García Garzón <david.garcia@upf.edu>
2015-08-09 16:19:28 +0200

    ngmin deprecated by ng-annotate

--- Gruntfile.js
+++ Gruntfile.js
@@ -303,10 +303,10 @@ module.exports = function (grunt) {
             }
         },
 
-        // ngmin tries to make the code safe for minification automatically by
+        // ng-annotate tries to make the code safe for minification automatically by
         // using the Angular long form for dependency injection. It doesn't work on
         // things like resolve or inject so those have to be done manually.
-        ngmin: {
+        ngAnnotate: {
             dist: {
                 files: [
                     {
@@ -456,7 +456,7 @@ module.exports = function (grunt) {
         'concurrent:dist',
         'autoprefixer',
         'concat',
-        'ngmin',
+        'ngAnnotate',
         'copy:dist',
         'cdnify',
         'cssmin',
