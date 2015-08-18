2fe5c6198902f411b8348ae15be6ca64fe6d5ef7
David García Garzón <voki@canvoki.net>
2015-08-18 12:35:51 +0200

    Grunt: Distributing html views in components

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
                     livereload: '<%= connect.options.livereload %>'
                 },
                 files: [
-                    '<%= yeoman.app %>/{,*/}*.html',
+                    '<%= yeoman.app %>/{,*/}{,*/}*.html',
                     '.tmp/styles/{,*/}*.css',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
                 ]
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost',
+                hostname: '0.0.0.0',
                 livereload: 35729
             },
             livereload: {
@@ -296,7 +296,7 @@ module.exports = function (grunt) {
                     {
                         expand: true,
                         cwd: '<%= yeoman.dist %>',
-                        src: ['*.html', 'views/{,*/}*.html'],
+                        src: ['*.html', 'views/{,*/}*.html', 'scripts/components/*.html'],
                         dest: '<%= yeoman.dist %>'
                     }
                 ]
@@ -340,6 +340,7 @@ module.exports = function (grunt) {
                             '.htaccess',
                             '*.html',
                             'views/{,*/}*.html',
+                            'scripts/components/*.html',
                             'images/{,*/}*.{webp}',
                             'fonts/*',
                             'styles/fonts/{,*/}*.*'
