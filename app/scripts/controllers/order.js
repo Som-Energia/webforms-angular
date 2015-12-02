32dcda3da7cf9e9648b6823717cea3c70e8dd72c
David García Garzón <voki@canvoki.net>
2015-12-02 19:36:59 +0100

    properly sending multiple file input field

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -365,7 +365,10 @@ angular.module('newSomEnergiaWebformsApp')
             formData.append('cups_municipi', $scope.form.city.id);
             formData.append('referencia', $scope.cadastreEditor.value || '');
             formData.append('fitxer', $scope.form.invoice.file().files[0]);
-            formData.append('documentacio_alta', $scope.form.documentation.file().files);
+            var documentationFiles = $scope.form.documentation.file();
+            jQuery.each(documentationFiles.files, function(j, file) {
+                formData.append('documentacio_alta', file);
+            });
             formData.append('payment_iban', $scope.getCompleteIban());
             formData.append('escull_pagador', $scope.form.choosepayer);
             formData.append('compte_tipus_persona', $scope.payer.usertype === 'person' ? 0 : 1);
